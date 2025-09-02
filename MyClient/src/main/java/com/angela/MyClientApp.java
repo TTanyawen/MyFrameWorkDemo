a818d8027e16d342e235ec46e7f45a2ecff06c48
Tan Yawen <2749934612@qq.com>
2025-09-02 19:48:43 +0800

    init:MyClient调用者初始化

---
+++ MyClient/src/main/java/com/angela/MyClientApp.java
@@ -0,0 +1,11 @@
+package com.angela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyClientApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MyClientApp.class, args);
+    }
+}
